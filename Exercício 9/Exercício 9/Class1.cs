30cf60d1909ba5082d6e1de4a191a14e705a75e6
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:26:30 -0300

    Add files via upload

---
+++ Exercício 9/Exercício 9/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_9
+{
+    class Numero
+    {
+        double valor;
+
+        public void SetValor(double p)
+        {
+            valor = p;
+        }
+
+        public double GetValor()
+        {
+            return valor;
+        }
+
+        public int Comparar(double p)
+        {
+            int k;
+            if (valor > p)
+            {
+                k = 1;
+                return k;
+            }
+            else if (valor < p)
+            {
+                k = -1;
+                return k;
+            }
+            else
+            {
+                k = 0;
+                return k;
+            }
+
+        }
+    }
+}
---
+++ Exercício 9/Exercício 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercício_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Numero();
+            var a2 = new Numero();
+
+            Console.Write("Informe o primeiro número: ");
+            a1.SetValor(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o segundo número: ");
+            a2.SetValor(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine((a1.Comparar(a2.GetValor()) == 1) ? "O primeiro número é maior que o segundo"
+            : (a1.Comparar(a2.GetValor()) == -1) ? "O segundo número é maior que o primeiro"
+            : "Os dois números são idênticos");
+        }
+    }
+}
